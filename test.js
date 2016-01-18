022df000258aa7fdb059503a25a4cb3350a03487
Javier Collado <javier.collado@gmail.com>
2016-01-18 01:43:51 +0100

    test(index): Add promisify function test cases
    Add test cases to make sure parameters are handled correctly and also that promise is
    resolved/rejected properly

---
+++ test.js
@@ -0,0 +1,39 @@
+/* global describe it */
+'use strict'
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+
+const expect = chai.expect
+
+const promisify = require('.')
+
+describe('promisify', function () {
+  it('calls wrapped function with arguments', function () {
+    const fn = sinon.stub().yields()
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.be.eventually.fulfilled.then(function () {
+        expect(fn).to.have.been.calledWith('some', 'arguments')
+      })
+  })
+
+  it('rejects when wrapped function returns error', function () {
+    const fn = sinon.stub().yields('some error')
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.be.eventually.rejectedWith('some error')
+  })
+
+  it('resolves when wrapped function returns no error', function () {
+    const fn = sinon.stub().yields(null, 'a', 'result')
+    const promisified = promisify(fn)
+    return expect(promisified('some', 'arguments'))
+      .to.eventually.deep.equal(['a', 'result'])
+  })
+})
