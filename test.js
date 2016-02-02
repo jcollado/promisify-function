f8ea7e2f3e182a7beb97433ac9cbaec8ea3956e4
Javier Collado <javier.collado@gmail.com>
2016-02-02 16:06:14 +0100

    refactor(test): Update test code to es6
    Test code is transpiled on the fly, so it can use es6 code

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
 /* global describe it */
-'use strict'
-
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
-const sinon = require('sinon')
-const sinonChai = require('sinon-chai')
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 
 chai.use(chaiAsPromised)
 chai.use(sinonChai)
@@ -13,7 +11,7 @@ const expect = chai.expect
 
 const promisify = require('./src')
 
-describe('promisify', function () {
+describe('promisify', () => {
   it('calls wrapped function with arguments', function () {
     const fn = sinon.stub().yields()
     const promisified = promisify(fn)
@@ -23,35 +21,35 @@ describe('promisify', function () {
       })
   })
 
-  it('rejects when wrapped function returns error', function () {
+  it('rejects when wrapped function returns error', () => {
     const fn = sinon.stub().yields('some error')
     const promisified = promisify(fn)
     return expect(promisified('some', 'arguments'))
       .to.be.eventually.rejectedWith('some error')
   })
 
-  it('resolves when wrapped function returns no error', function () {
+  it('resolves when wrapped function returns no error', () => {
     const fn = sinon.stub().yields(null, 'a', 'result')
     const promisified = promisify(fn)
     return expect(promisified('some', 'arguments'))
       .to.eventually.be.fulfilled
   })
 
-  it('resolves single value as such', function () {
+  it('resolves single value as such', () => {
     const fn = sinon.stub().yields(null, 42)
     const promisified = promisify(fn)
     return expect(promisified('some', 'arguments'))
       .to.eventually.deep.equal(42)
   })
 
-  it('resolves multiple value as an array', function () {
+  it('resolves multiple value as an array', () => {
     const fn = sinon.stub().yields(null, 'a', 'result')
     const promisified = promisify(fn)
     return expect(promisified('some', 'arguments'))
       .to.eventually.deep.equal(['a', 'result'])
   })
 
-  it('throws when no function is passed', function () {
+  it('throws when no function is passed', () => {
     expect(promisify).to.throw(Error, 'fn parameter must be a function')
   })
 })
